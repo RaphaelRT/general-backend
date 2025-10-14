1d9db73e4e950204b0bcffd337794a067d24f9f7
root <raphael.robert@hetic.net>
2025-10-14 13:06:17 +0000

    fix: database-name

--- src/modules/intervu/db.ts
+++ src/modules/intervu/db.ts
@@ -1,5 +1,10 @@
 import { PrismaClient as IntervuClient } from "@prisma/intervu-client";
+import { env } from "../../config/env";
 
-export const intervuPrisma = new IntervuClient();
+export const intervuPrisma = new IntervuClient({
+  datasources: {
+    db: { url: env.intervuDbUrl }
+  }
+});
 
 
--- src/modules/portfolio/db.ts
+++ src/modules/portfolio/db.ts
@@ -1,5 +1,10 @@
 import { PrismaClient as PortfolioClient } from "@prisma/portfolio-client";
+import { env } from "../../config/env";
 
-export const portfolioPrisma = new PortfolioClient();
+export const portfolioPrisma = new PortfolioClient({
+  datasources: {
+    db: { url: env.portfolioDbUrl }
+  }
+});
 
 
